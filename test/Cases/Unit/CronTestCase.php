90e40e1191ef685cad02e4fb778fc5018fb816a6
mehr-it <mail@mehr-it.info>
2019-10-24 10:55:24 +0200

    FIX: add void return for setup method

--- test/Cases/Unit/CronTestCase.php
+++ test/Cases/Unit/CronTestCase.php
@@ -16,7 +16,7 @@
 		/**
 		 * @inheritDoc
 		 */
-		protected function setUp() {
+		protected function setUp(): void {
 			parent::setUp();
 
 			$this->timezone = new \DateTimeZone('Europe/Berlin');
